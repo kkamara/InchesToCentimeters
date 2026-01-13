aceea57c3afb7b1876eb5e63f4b404b3e5010b44
kkamara <kamaracomputers@gmail.com>
2026-01-13 13:17:07 +0000

    Connect MainActivity to layout components by IDs

--- app/src/main/java/com/kelvinkamara/inchestocentimeters/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/inchestocentimeters/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.kelvinkamara.inchestocentimeters
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val inchValue = 2.54
+    private lateinit var enterInches: EditText
+    private lateinit var convertButton: Button
+    private lateinit var textViewCentimeters: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        enterInches = findViewById(R.id.editTextInches)
+        convertButton = findViewById(R.id.button_convert)
+        textViewCentimeters = findViewById(R.id.textViewConvert)
+
+//        convertButton.setOnClickListener {}
     }
 }
